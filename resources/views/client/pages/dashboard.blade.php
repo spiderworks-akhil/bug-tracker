69399820984416738a08ab1951bf7f5da83018fb
Akhil Joy <b2akhilmj@gmail.com>
2021-06-01 13:03:40 +0530

    dashboard and login

--- resources/views/client/pages/dashboard.blade.php
+++ resources/views/client/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('client.base')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
 
             <table class="table table-striped">
